a69548ca59c0d8c3997aec9d993553c8311a53a2
Arpit045-boop <56675826+Arpit045-boop@users.noreply.github.com>
2022-01-18 22:11:31 +0530

    Time: 30 ms (20.12%), Space: 20.3 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,60 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(n==0){
-            return true;
-        }
-        for(int i=0;i<flowerbed.size();){
-            if(flowerbed[i]==0){
-                if(i-1>=0 and i+1<flowerbed.size()){
-                    if(flowerbed[i-1]==0 and flowerbed[i+1]==0){
-                        i+=2;
-                        n--;
-                        if(n==0){
-                            return true;
-                        }
-                    }
-                    else{
-                        i+=1;
-                    }
-                }
-                else if(i-1<0 and i+1>=flowerbed.size()){
-                    i+=2;
-                    n--;
-                    if(n==0){
-                        return true;
-                    }
-                }
-                else if(i-1<0){
-                    if(flowerbed[i+1]==0){
-                        i+=2;
-                        n--;
-                        if(n==0){
-                            return true;
-                        }
-                    }
-                    else{
-                        i+=1;
-                    }
-                }
-                else if(i+1>=flowerbed.size()){
-                    if(flowerbed[i-1]==0){
-                        i+=2;
-                        n--;
-                        if(n==0){
-                            return true;
-                        }
-                    }
-                    else{
-                        i+=1;
-                    }
-                }
+        int zero=1;
+        int ans=0;
+        for(int f:flowerbed){
+            if(f==0){
+                zero++;
             }
             else{
-                i+=1;    
+                ans+=(zero-1)/2;
+                zero=0;
             }
-            
         }
-        return false;
+        return (ans + zero/2)>=n;
     }
 };
