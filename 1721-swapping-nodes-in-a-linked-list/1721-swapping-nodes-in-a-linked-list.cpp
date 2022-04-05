f6c7b3f9d6bddaa1684dc2426f2bc178256dbeca
Arpit045-boop <56675826+Arpit045-boop@users.noreply.github.com>
2022-04-05 12:19:49 +0530

    Time: 986 ms (33.60%), Space: 179.8 MB (99.62%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,26 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* swapNodes(ListNode* head, int k1) {
-        ListNode* left = head;
-        ListNode* right = head;
-        ListNode* curr = head;
-        
-        int counter = 1;
-        while(curr!=NULL){
-            if (counter<k1){
-                left = left->next;
-            }
-            if (counter>k1){
-                right = right->next;
-            }
-            curr = curr->next;
-            counter++;
+    ListNode* swapNodes(ListNode* head, int k) {
+        int n=0;
+        ListNode *ptr = head;
+        while(ptr){
+            n++;
+            ptr=ptr->next;
         }
-        //swap values
-        int temp = left->val;
-        left->val = right->val;
-        right->val = temp;
+        ListNode* temp= head;
+        ListNode* pre = NULL;
+        int i=1;
         
+        while(i<k){
+            pre=temp;
+            temp = temp->next;
+            i++;
+        }
+        ListNode* cur = temp;
+        temp = head;
+        int j=1;
+        while(j<(n-k+1)){
+            temp=temp->next;
+            j++;
+        }
+        // ListNode* dummy=cur->next;
+        int temp1;
+        temp1=temp->val;
+        temp->val = cur->val;
+        cur->val = temp1;
         return head;
+        
     }
 };
