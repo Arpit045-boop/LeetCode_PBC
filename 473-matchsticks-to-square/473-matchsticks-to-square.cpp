80d17d3b45726018e732731264bc21f0b4c6560b
Arpit045-boop <56675826+Arpit045-boop@users.noreply.github.com>
2022-07-12 15:55:11 +0530

    Time: 934 ms (17.18%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
-    int a,b,c,d;
-    bool func(vector<int> &v, int i){
-        if(i==v.size()){
-            if(a==0 && b==0 && c==0 && d==0){
-                return true;
-            }
+    bool func(int k , vector<int> & v, vector<int> &visited, int i, int cur_sum , int target){
+        if(k==1){
+            return true;
+        }
+        if(cur_sum == target){
+            return func(k-1, v,visited, 0,0,target);
+        }
+        
+        for(int j=i;j<v.size();j++){
+            if(visited[j] || cur_sum + v[j] > target) continue;
             
-            else{
-                return false;
-            }
+            visited[j] = true;
+            if(func(k,v,visited,j+1,cur_sum+v[j] , target)) return true;
+            visited[j]=false;
         }
-        
-        if(v[i] <= a){
-            a -= v[i];
-            if(func(v,i+1)) return true;
-            a += v[i];
-        }
-        if(v[i] <=b){
-            b -= v[i];
-            if(func(v,i+1)) return true;
-            b += v[i];
-        }
-        if(v[i] <= c){
-            c -= v[i];
-            if(func(v,i+1)) return true;
-            c += v[i];
-        }
-        if(v[i] <= d){
-            d -= v[i];
-            if(func(v,i+1)) return true;
-            d += v[i];
-        }
-        
         return false;
+        
     }
-    
     
     bool makesquare(vector<int>& matchsticks) {
         int size = matchsticks.size();
-        if(size<4){
-            return false;
-        }
-        
-        int sum =0;
+        vector<int> visited (size, false);
+        int sum = 0;
         for(int i=0;i<size;i++){
-            sum+=matchsticks[i];
+            sum += matchsticks[i];
         }
-        
-        if( sum % 4!=0){
-            return false;
-        }
-        a = sum/4 , b = sum/4 , c = sum/4 , d= sum/4;
-        sort(matchsticks.rbegin(),matchsticks.rend());
-        
-        return func(matchsticks,0);
-        
+        if(size<4 || sum % 4) return false;
+        return func(4,matchsticks,visited,0,0,sum/4);
     }
 };
